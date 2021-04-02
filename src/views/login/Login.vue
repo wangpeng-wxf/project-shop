b91346351a1edda72e583324126f67475a023de3
wangpeng-js <wangpeng-js@yihuacomputer.com>
2021-04-02 21:38:48 +0800

    init project

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>
+        登录
+    </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+    name: 'Login'
 }
 </script>
-
 <style lang="less" scoped>
 
 </style>
